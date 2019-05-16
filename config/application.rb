5aa5b7918d084c4d4e6da7afdb8bb95e84c27482
wintop001 <davidcdx@163.com>
2019-05-16 12:01:38 +0800

    add user following

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -14,5 +14,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # 在使用 Ajax 处理的表单中添加真伪令牌
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
